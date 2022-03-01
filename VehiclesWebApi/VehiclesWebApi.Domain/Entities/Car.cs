8e52191a148d1cc5648c4084e05a4be68c1b4b23
vaibhav-shah1988 <vaibhav_shah1988@yahoo.com>
2022-03-02 01:11:16 +0530

    Added Cars Objects

---
+++ VehiclesWebApi/VehiclesWebApi.Domain/Entities/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace VehiclesWebApi.Domain.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public int Speed { get; set; }
+        public double MalfunctionChance { get; set; }
+        public int MalfunctionOccured { get; set; }
+        public int DistaanceCoveredInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedForHours { get; set; }
+
+    }
+}
---
+++ VehiclesWebApi/VehiclesWebApi/Features/Cars/CarsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VehiclesWebApi.Domain.Entities;
+
+namespace VehiclesWebApi.Features.Cars
+{
+    [Route("api/cars")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Car>> GetCars()
+        {
+            var cars = new List<Car>();
+            cars.Add(new Car() { TeamName = "Team1", Speed = 100, MalfunctionChance = 0.3 });
+            cars.Add(new Car() { TeamName = "Team2", Speed = 110, MalfunctionChance = 0.4 });
+            cars.Add(new Car() { TeamName = "Team3", Speed = 95, MalfunctionChance = 0.2 });
+
+            return Ok(cars);
+        }
+    }
+}
