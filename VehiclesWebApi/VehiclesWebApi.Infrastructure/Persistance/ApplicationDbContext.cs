b991ceb36962fe68d0b2f99f2886abaceca64694
vaibhav-shah1988 <vaibhav_shah1988@yahoo.com>
2022-03-05 00:28:06 +0530

    added dependency

---
+++ VehiclesWebApi/VehiclesWebApi.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehiclesWebApi.Domain.Entities;
+
+namespace VehiclesWebApi.Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
--- VehiclesWebApi/VehiclesWebApi/Startup.cs
+++ VehiclesWebApi/VehiclesWebApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using VehiclesWebApi.Infrastructure;
 
 namespace VehiclesWebApi
 {
@@ -19,6 +20,7 @@ namespace VehiclesWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
